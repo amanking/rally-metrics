9ab1a4d43ec60883d2ab953636ac290de85e1041
Tushar Madhukar <tushar.madhukar@gmail.com>
2013-05-07 22:40:55 +0530

    refactored unit_of_work, completed pending tests

--- lib/unit_of_work.rb
+++ lib/unit_of_work.rb
@@ -7,32 +7,39 @@ class UnitOfWork
     @id = id
     @name = name
     @estimate = estimate.to_f
-    @timeline = timeline
+    @time_spent = time_spent_per_stage(timeline)
   end
 
   def dev_days
-    days_between(:dev, :qa)
+    days_in(:dev)
   end
 
   def qa_days
-    days_between(:qa, :done)
+    days_in(:qa)
   end
 
   def to_s
     <<-STR
       Id : #{@id}
       Name : #{@name}
-      Timeline:
-      #{@timeline.join("\n")}
+      Time spent: #{@time_spent.map { |t| "{#{t[0]} : #{t[1]}}" }.join(' , ')}
     STR
   end
 
   private
 
-  def days_between(act1, act2)
-    start = @timeline.find { |act| act[0] == act1 }
-    finish = @timeline.find { |act| act[0] == act2 }
-    date_diff_rounded(finish[1], start[1])
+  def days_in(stage)
+    @time_spent.find_all { |tuple| tuple[0] == stage }.inject(0) { |sum, tuple| sum + tuple[1] }
+  end
+
+  def time_spent_per_stage(timeline)
+    sorted_timeline = timeline.sort_by { |t| t[1] }
+
+    dates = sorted_timeline.collect { |t| t[1] }
+    shifted_dates = dates.push(DateTime.now).drop(1)
+
+    time_spent = shifted_dates.zip(dates).map { |pair| date_diff_rounded(pair[0], pair[1]) }
+    sorted_timeline.collect { |t| t[0] }.zip(time_spent)
   end
 
   def date_diff_rounded(date1, date2)
--- spec/unit/unit_of_work_spec.rb
+++ spec/unit/unit_of_work_spec.rb
@@ -1,10 +1,57 @@
 require File.join(File.dirname(__FILE__), "../spec_helper")
-require "unit_of_work"
+require 'unit_of_work'
+require 'date'
+
+
+def approx_equal(float_val)
+  be_within(0.01).of(float_val)
+end
 
 describe "A Unit of work" do
-  it "should return correct dev days even if the task moved from dev to done"
-  it "should return dev days even if the task is still in progress"
-  it "should return dev days as 0 if a task never went in dev"
+  it "should return dev days" do
+    work = UnitOfWork.new('', '', '', [
+        [:dev, DateTime.iso8601('2013-04-12T12:00:01Z')],
+        [:qa, DateTime.iso8601('2013-04-13T12:00:01Z')]
+    ])
+
+    work.dev_days.should approx_equal(1.0)
+  end
+
+  it "should return correct dev days even if the task moved from dev to done" do
+    work = UnitOfWork.new('', '', '', [
+        [:dev, DateTime.iso8601('2013-04-12T12:00:01Z')],
+        [:done, DateTime.iso8601('2013-04-16T12:00:01Z')]
+    ])
+
+    work.dev_days.should approx_equal(4.0)
+  end
+
+  it "should return dev days even if the task is still in progress" do
+    work = UnitOfWork.new('', '', '', [
+        [:dev, DateTime.iso8601('2013-04-12T12:00:01Z')]
+    ])
+
+    work.dev_days.should approx_equal(25.2)
+  end
+
+  it "should return dev days as 0 if a task never went in dev" do
+    work = UnitOfWork.new('', '', '', [])
+    work.dev_days.should approx_equal(0.0)
+  end
+
+  it "should return dev days combined if it went in dev multiple times" do
+    work = UnitOfWork.new('', '', '', [
+        [:dev, DateTime.iso8601('2013-04-12T12:00:01Z')],
+        [:qa, DateTime.iso8601('2013-04-16T12:00:01Z')],
+        [:dev, DateTime.iso8601('2013-04-16T22:00:01Z')],
+        [:done, DateTime.iso8601('2013-04-18T12:00:01Z')]
+    ])
+
+    work.dev_days.should approx_equal(5.6)
+  end
+
   it "should return qa days even if the task is still in progress"
   it "should return qa days as 0 if a task never went in qa"
+  it "should return qa days combined if it went in qa multiple times"
+
 end
